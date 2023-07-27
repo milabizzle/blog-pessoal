753da009f74be2e6059dff707b3654183b3c19b1
milabizzle <parcerias.cr@gmail.com>
2023-07-27 11:37:44 -0300

    Implementação de rotas e estilização + consumo do backend

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,31 @@
+
+import Navbar from './components/navBar/NavBar';
+import Footer from './components/footer/Footer';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from './Pages/login/Login';
+import Cadastro from './Pages/cadastro/Cadastro';
 import Home from './Pages/home/Home';
-import './App.css';
-import React from 'react';
+import { AuthProvider } from './contexts/AuthContext';
+
 
 function App() {
- 
   return (
     <>
-      <Home/>
+    <AuthProvider>
+        <BrowserRouter>
+          <Navbar />
+          <div className='min-h-[80vh]'>
+            <Routes>
+              <Route path="/" element={<Login />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/cadastro" element={<Cadastro />} />
+              <Route path="/home" element={<Home />} />
+            </Routes>
+          </div>
+          <Footer />
+        </BrowserRouter>
+        </AuthProvider>
     </>
   );
 }
-
 export default App;
