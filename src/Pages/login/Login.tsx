16d55f981610bb2e2b0c0854b0b0e4cbb6ade3dd
milabizzle <parcerias.cr@gmail.com>
2023-08-02 11:21:42 -0300

    Implementando o CRUD de Postagens

--- src/Pages/login/Login.tsx
+++ src/Pages/login/Login.tsx
@@ -39,7 +39,7 @@ function login(e: ChangeEvent<HTMLFormElement>) {
   return (
     <>
       <div className="grid grid-cols-1 lg:grid-cols-2 h-screen place-items-center font-bold ">
-        <form className="flex justify-center items-center flex-col w-1/2 gap-4" onSubmit={login}>
+        <form onSubmit={login} className="flex justify-center items-center flex-col w-1/2 gap-4" >
           <h2 className="text-slate-900 text-5xl ">Entrar</h2>
           <div className="flex flex-col w-full">
             <label htmlFor="usuario">Usuário</label>
