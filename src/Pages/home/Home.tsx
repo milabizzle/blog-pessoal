fa2e15364b5c3022bf6f5b1fcdeb6733c1d96512
milabizzle <parcerias.cr@gmail.com>
2023-08-11 15:03:27 -0300

    Atualização de Style na Home

--- src/Pages/home/Home.tsx
+++ src/Pages/home/Home.tsx
@@ -9,15 +9,15 @@ import ModalPostagem from '../../components/postagens/modalPostagem/ModalPostage
 function Home() {
     return (
         <>
-        <div className="bg-sky-800 flex justify-center">
+        <div className="bg-gray-900 flex justify-center">
           <div className='container grid grid-cols-2 text-white'>
             <div className="flex flex-col gap-4 items-center justify-center py-4">
-              <h2 className='text-5xl font-bold'>Seja bem vinde!</h2>
-              <p className='text-xl'>Expresse aqui seus pensamentos e opniões</p>
+              <h2 className='text-5xl font-bold drop-shadow-lg'>Seja bem vinde!</h2>
+              <p className='text-xl drop-shadow-lg'>Expresse aqui seus pensamentos e opniões</p>
   
               <div className="flex justify-around gap-4">
               <ModalPostagem />
-              <button className='outline-double rounded border-x-indigo-600 bg-white text-blue-900 py-2 px-4'>Ver postagens</button>
+              <button className='rounded shadow-lg shadow-indigo-500/40 bg-white text-blue-900 py-2 px-4'>Ver postagens</button>
             </div>
             </div>
   
